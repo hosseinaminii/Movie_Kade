71c38ee16558e0dea913afd5133b0e66fb96cf58
Hossein Amini <hosein_amini@ymail.com>
2017-02-12 11:27:56 +0330

    Add toolbar for SettingActivity

--- app/src/main/java/com/aminiam/moviekade/activity/SettingActivity.java
+++ app/src/main/java/com/aminiam/moviekade/activity/SettingActivity.java
@@ -1,15 +1,37 @@
 package com.aminiam.moviekade.activity;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.aminiam.moviekade.R;
+import com.aminiam.moviekade.databinding.ActivitySettingBinding;
 
 public class SettingActivity extends AppCompatActivity {
+
+    private ActivitySettingBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_setting);
+        setSupportActionBar(mBinding.toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
