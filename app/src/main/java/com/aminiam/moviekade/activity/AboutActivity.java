12c81bf76bd22a78bbd856005fcdf51b9bd7c739
Hossein Amini <hosein_amini@ymail.com>
2017-02-12 16:28:02 +0330

    Create about activity

--- app/src/main/java/com/aminiam/moviekade/activity/AboutActivity.java
+++ app/src/main/java/com/aminiam/moviekade/activity/AboutActivity.java
@@ -1,16 +1,37 @@
 package com.aminiam.moviekade.activity;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.aminiam.moviekade.R;
+import com.aminiam.moviekade.databinding.ActivityAboutBinding;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private ActivityAboutBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_about);
+        setSupportActionBar(mBinding.toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
